3bb6afd451ce56ad1a8a066987120dba9329e337
pecoraroFRANCESCO <consenso86@hotmail.com>
2019-01-21 21:54:37 +0100

    tj pas ok

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+#!/usr/bin/env node
+
+const axios = require('axios');
+var validator = require("email-validator");
+
+var mail = process.argv[2];
+
+if(validator.validate(mail)){
+	console.log('email OK');
+	axios({
+		method: 'get',
+		url: `https://haveibeenpwned.com/api/${mail}`,
+		header:{
+			'api-version': '2',
+			'user-agent': "nondidju"
+		}
+	})
+		.then(function(reponse){
+			console.log(reponse.data.Name)
+
+		})
+		.catch(function(error){
+			console.log(error)
+
+		})
+
+}else{
+	console.log('email not OK');
+}
 
 
-console.log("Hello, Node.JS!");
+
