450db047c7e4dc04cce43fb094eb8be53e8f2139
pecoraroFRANCESCO <consenso86@hotmail.com>
2019-01-22 11:48:59 +0100

    eee

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 #!/usr/bin/env node
 
 const axios = require('axios');
-var validator = require("email-validator");
+const validator = require("email-validator");
+const ora = require('ora');
+const chalk = require('chalk');
+const figlet = require('figlet');
 
-var mail = process.argv[2];
 
-if(validator.validate(mail)){
-	console.log('email OK');
+let mail = process.argv[2];
+
+const spinner = ora('Loading unicorns');
+
+
+if(validator.validate(mail)){	
+	console.log('email OK'+' => '+mail);
+	spinner.start();
+	setTimeout(() => {
+	spinner.color = 'yellow';
+	spinner.text = 'Loading rainbows';
 	axios({
 		method: 'get',
 		url: `https://haveibeenpwned.com/api/v2/breachedaccount/${mail}`,
-		header:{
-			'api-version': '2',
-			'user-agent': "mynodejs"
+		headers:{
+			'User-Agent': "mynodejs"
 		}
 	})
-		.then(function(reponse){
-			console.log(reponse.data.Name)
+		.then(function(response){
+			var bo = response.data.map(({Name}) => Name);
+			console.log(chalk.red(chalk.bgYellow(bo)));
 
 		})
 		.catch(function(error){
-			console.log(error)
+			if(error.response.status == 404){
+				console.log(chalk.yellow(chalk.bgRed('votre adresse est clean !')));
+			}
+			if(error.status == 403){
+				console.log('API error');
+			}
 
 		})
-
+		spinner.stop();
+	}, 1000);
 }else{
 	console.log('email not OK');
 }
